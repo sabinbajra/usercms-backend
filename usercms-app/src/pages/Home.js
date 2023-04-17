32099a5c34db322375cc878c17a67b719e1639ba
sabin bajracharya <sabin1129@hotmail.com>
2023-04-17 14:02:37 +0200

    Fetch all user data from database using axios

--- usercms-app/src/pages/Home.js
+++ usercms-app/src/pages/Home.js
@@ -1,6 +1,20 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 const Home = () => {
+
+    const [users,setUsers] = useState([]);
+
+    useEffect(() => {
+      loadUsers();  
+    },[]);
+
+    const loadUsers = async () => {
+        const result = await axios.get("http://localhost:8080/users");
+        console.log(result.data);
+        setUsers(result.data);
+    }
+
+
   return (
     <div className='container'>
         <div className="py-4">
@@ -11,27 +25,26 @@ const Home = () => {
                 <th scope="col">Name</th>
                 <th scope="col">UserName</th>
                 <th scope="col">Email</th>
+                <th scope="col">Action</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-                </tr>
-                <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-                </tr>
-                <tr>
-                <th scope="row">3</th>
-                <td>Larry</td>
-                <td>the Bird</td>
-                <td>@twitter</td>
-                </tr>
+                {
+                    users.map( (user,index) => {
+                        return (
+                            <tr key={(user.id)}>
+                            <th scope="row">{user.id}</th>
+                            <td>{user.name}</td>
+                            <td>{user.username}</td>
+                            <td>{user.email}</td>
+                            <td>TO DO something</td>
+                            </tr>
+                        )
+
+                    } )
+                }
+               
+                
             </tbody>
         </table>
         </div>
