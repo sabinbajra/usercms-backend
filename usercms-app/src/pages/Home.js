6ad996917b69c966857f4d2f92cc02e98422c0e9
sabin bajracharya <sabin1129@hotmail.com>
2023-04-17 12:42:20 +0200

    Home page with table

--- usercms-app/src/pages/Home.js
+++ usercms-app/src/pages/Home.js
@@ -2,7 +2,40 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>This is the home page</div>
+    <div className='container'>
+        <div className="py-4">
+        <table className=" table table-bordered ">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">UserName</th>
+                <th scope="col">Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                </tr>
+                <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                </tr>
+                <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+                </tr>
+            </tbody>
+        </table>
+        </div>
+    </div>
   )
 }
 
