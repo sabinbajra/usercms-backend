4f37702e324e2dd380cc56f29b2484b3096018ad
sabin bajracharya <sabin1129@hotmail.com>
2023-04-17 14:59:43 +0200

    added footer

--- usercms-app/src/App.js
+++ usercms-app/src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Navbar from './layout/Navbar';
 import Home from './pages/Home';
+import Footer from './layout/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Home/>
+      <Footer/>
     </div>
   );
 }
---
+++ usercms-app/src/layout/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+
+<div className="footer"><strong>Copyright @ Sabin Bajracharya 2023 USER CMS</strong></div>
+  )
+}
+
+export default Footer
