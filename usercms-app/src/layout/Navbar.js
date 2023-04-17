f1c3cafd8eb22b1dc0316c1a15c73856f8c2dbe3
sabin bajracharya <sabin1129@hotmail.com>
2023-04-17 12:15:25 +0200

    Navbar added

--- usercms-app/src/layout/Navbar.js
+++ usercms-app/src/layout/Navbar.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from 'react';
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div className="container-fluid">
+            <a class="navbar-brand" href="#">FULL STACK USERCMS</a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <button className="btn btn-outline-light" to="/adduser">
+            Add User
+          </button>
+        </div>
+      </nav>
+    </div>
   )
 }
 
